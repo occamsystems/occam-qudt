d167bb354fa99b6d9645dc3f64eb2af26e9787b1
Joshua Feingold <josh@occamsystems.com>
2024-09-13 10:53:03 -0400

    Add some quantity math functions.

--- core/src/main/java/com/occamsystems/qudt/QuantityValue.java
+++ core/src/main/java/com/occamsystems/qudt/QuantityValue.java
@@ -20,6 +20,44 @@ public class QuantityValue {
     this.unit = unit;
   }
 
+  public double value() {
+    return this.unit.scale(unscaled);
+  }
+
+  public static QuantityValue divide(QuantityValue qv1, QuantityValue qv2) {
+    return new QuantityValue(qv1.unscaled / qv2.unscaled,
+        new AggregateUnit(qv1.unit, 1, qv2.unit, -1));
+  }
+
+  public static QuantityValue add(QuantityValue qv1, QuantityValue qv2) {
+    assert qv1.unit.isConvertable(qv2.unit);
+
+    return new QuantityValue(qv1.unscaled + qv2.unscaled,
+        qv1.unit);
+  }
+
+
+  public static QuantityValue subtract(QuantityValue qv1, QuantityValue qv2) {
+    assert qv1.unit.isConvertable(qv2.unit);
+
+    return new QuantityValue(qv1.unscaled - qv2.unscaled,
+        qv1.unit);
+  }
+
+  public static QuantityValue pow(QuantityValue qv1, int pow) {
+    return new QuantityValue(Math.pow(qv1.unscaled, pow),
+        new AggregateUnit(qv1.unit, pow));
+  }
+
+  public static QuantityValue pow(QuantityValue qv1, SmallFraction pow) {
+    return new QuantityValue(Math.pow(qv1.unscaled, pow.doubleValue()),
+        new AggregateUnit(qv1.unit, pow));
+  }
+
+  public static QuantityValue pow(QuantityValue qv1, int powNum, int powDenom) {
+    return QuantityValue.pow(qv1, new SmallFraction(powNum, powDenom));
+  }
+
   public static QuantityValue multiply(QuantityValue qv1, QuantityValue qv2) {
     return new QuantityValue(qv1.unscaled * qv2.unscaled,
         new AggregateUnit(qv1.unit, 1, qv2.unit, 1));
