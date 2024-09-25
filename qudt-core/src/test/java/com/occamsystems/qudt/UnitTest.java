65ff07094d79fe27606ee9dba13e3b0b8d5400c7
Joshua Feingold <josh@occamsystems.com>
2024-09-25 12:04:03 -0400

    Simple unit test for number checks.

---
+++ qudt-core/src/test/java/com/occamsystems/qudt/UnitTest.java
@@ -0,0 +1,20 @@
+package com.occamsystems.qudt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.occamsystems.qudt.predefined.units.D1Units;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Copyright (c) 2024 Occam Systems, Inc.
+ */
+class UnitTest {
+  @Test
+  void numbers() {
+    Assertions.assertTrue(D1Units.UNITLESS.u.isNumber());
+    Assertions.assertTrue(D1Units.UNITLESS.u.isBasicNumber());
+    Assertions.assertTrue(D1Units.PERCENT.u.isNumber());
+    Assertions.assertFalse(D1Units.PERCENT.u.isBasicNumber());
+  }
+}
